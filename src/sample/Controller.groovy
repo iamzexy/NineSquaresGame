6bf891487bec4b6816e0e7a547f4dd5b014aa395
Artem Demin <deminartem@bk.ru>
2021-08-05 00:16:17 +0500

    Working version 1.0.0

--- src/sample/Controller.groovy
+++ src/sample/Controller.groovy
@@ -1,62 +1,124 @@
-package sample;
+package sample
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
 import javafx.scene.control.Label
-import javafx.scene.control.ToggleButton;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.paint.Paint;
-
-import java.awt.*;
+import javafx.scene.control.ToggleButton
 
 class Controller {
     @FXML
-    private ToggleButton b11;
+    private ToggleButton b11
     @FXML
-    private Button b12;
+    private ToggleButton b12
     @FXML
-    private Button b13;
+    private ToggleButton b13
     @FXML
-    private Button b21;
+    private ToggleButton b21
     @FXML
-    private Button b22;
+    private ToggleButton b22
     @FXML
-    private Button b23;
+    private ToggleButton b23
     @FXML
-    private Button b31;
+    private ToggleButton b31
     @FXML
-    private Button b32;
+    private ToggleButton b32
     @FXML
-    private Button b33;
+    private ToggleButton b33
     @FXML
-    private Label labelWon;
+    private Label labelWon
+    @FXML
+    private List buttons
 
-    private Background defaultBg
-    private Background litBg
 
-    {
-        defaultBg = new Background(new BackgroundFill(Paint.valueOf("0x039ed322"), null, null))
-        litBg = new Background(new BackgroundFill(Paint.valueOf("BLUE"), null, null))
+    void initialize() {
+        buttons = [b11, b12, b13, b21, b22, b23, b31, b32, b33]
+        reset()
     }
-
-    void pressButton(ActionEvent e) {
-        changeColor(b11);
-        changeColor(b12);
-        changeColor(b21);
-//        b11.setBackground(new Background(new BackgroundFill(Paint.valueOf("BLUE"), null, null)));
-//        b12.setBackground(new Background(new BackgroundFill(Paint.valueOf("BLUE"), null, null)));
-//        b21.setBackground(new Background(new BackgroundFill(Paint.valueOf("BLUE"), null, null)));
+    void pressButton11(ActionEvent e) {
+        b12.isSelected() ? b12.setSelected(false) : b12.setSelected(true)
+        b21.isSelected() ? b21.setSelected(false) : b21.setSelected(true)
+        if (checkIfWon(e))
+            won(e)
+    }
+    void pressButton12(ActionEvent e) {
+        b11.isSelected() ? b11.setSelected(false) : b11.setSelected(true)
+        b13.isSelected() ? b13.setSelected(false) : b13.setSelected(true)
+        b22.isSelected() ? b22.setSelected(false) : b22.setSelected(true)
+        if (checkIfWon(e))
+            won(e)
+    }
+    void pressButton13(ActionEvent e) {
+        b12.isSelected() ? b12.setSelected(false) : b12.setSelected(true)
+        b23.isSelected() ? b23.setSelected(false) : b23.setSelected(true)
+        if (checkIfWon(e))
+            won(e)
+    }
+    void pressButton21(ActionEvent e) {
+        b11.isSelected() ? b11.setSelected(false) : b11.setSelected(true)
+        b22.isSelected() ? b22.setSelected(false) : b22.setSelected(true)
+        b31.isSelected() ? b31.setSelected(false) : b31.setSelected(true)
+        if (checkIfWon(e))
+            won(e)
+    }
+    void pressButton22(ActionEvent e) {
+        b12.isSelected() ? b12.setSelected(false) : b12.setSelected(true)
+        b21.isSelected() ? b21.setSelected(false) : b21.setSelected(true)
+        b23.isSelected() ? b23.setSelected(false) : b23.setSelected(true)
+        b32.isSelected() ? b32.setSelected(false) : b32.setSelected(true)
+        if (checkIfWon(e))
+            won(e)
+    }
+    void pressButton23(ActionEvent e) {
+        b13.isSelected() ? b13.setSelected(false) : b13.setSelected(true)
+        b22.isSelected() ? b22.setSelected(false) : b22.setSelected(true)
+        b33.isSelected() ? b33.setSelected(false) : b33.setSelected(true)
+        if (checkIfWon(e))
+            won(e)
+    }
+    void pressButton31(ActionEvent e) {
+        b21.isSelected() ? b21.setSelected(false) : b21.setSelected(true)
+        b32.isSelected() ? b32.setSelected(false) : b32.setSelected(true)
+        if (checkIfWon(e))
+            won(e)
+    }
+    void pressButton32(ActionEvent e) {
+        b31.isSelected() ? b31.setSelected(false) : b31.setSelected(true)
+        b22.isSelected() ? b22.setSelected(false) : b22.setSelected(true)
+        b33.isSelected() ? b33.setSelected(false) : b33.setSelected(true)
+        if (checkIfWon(e))
+            won(e)
+    }
+    void pressButton33(ActionEvent e) {
+        b23.isSelected() ? b23.setSelected(false) : b23.setSelected(true)
+        b32.isSelected() ? b32.setSelected(false) : b32.setSelected(true)
+        if (checkIfWon(e))
+            won(e)
     }
     void won(ActionEvent e) {
-        labelWon.setVisible(true);
-    }
-    void changeColor(Button b) {
-        if (b.getBackground().getFills().size()>1) {
-            b.setBackground(litBg)
-        } else {
-            b.setBackground(defaultBg)
+        labelWon.setVisible(true)
+        buttons.forEach() {ToggleButton btn ->
+            btn.setDisable(true)
         }
+    }
+
+    boolean checkIfWon(ActionEvent e) {
+        boolean isWon = true
+        buttons.forEach() {ToggleButton btn ->
+            btn.isSelected() ?: (isWon = false)
+        }
+        return isWon
+    }
+
+    void reset(ActionEvent e) {
+        buttons.forEach() {ToggleButton btn ->
+            btn.setSelected(false)
+            btn.setDisable(false)
+        }
+        labelWon.setVisible(false)
+        def rand = new Random()
+        def b1 = buttons.get(rand.nextInt(buttons.size()))
+        def b2 = buttons.get(rand.nextInt(buttons.size()))
+        b1.setSelected(true)
+        b2.setSelected(true)
     }
 }
